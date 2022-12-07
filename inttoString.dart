147a839cd0018efcfe399a8a437c60fb8defca05
Tanvir Ahammed <gxtanvir@gmail.com>
2022-12-07 11:28:15 +0600

    Create inttoString.dart

---
+++ inttoString.dart
@@ -0,0 +1,11 @@
+void main(){
+  double height = 5.7;
+  print("Height is $height");
+  //This will print datatype
+  print("Datatype of height is ${height.runtimeType}.");
+  //Convert to String
+  String heightinString = height.toString();
+  print("Height is $heightinString");
+  //This will print datatype
+  print("Data type of height is ${heightinString.runtimeType}.");
+}
