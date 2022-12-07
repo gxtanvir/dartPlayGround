1e289681632ae2f98f6979203b029660b9a34533
Tanvir Ahammed <gxtanvir@gmail.com>
2022-12-07 12:21:45 +0600

    Create stringtoDouble.dart

---
+++ stringtoDouble.dart
@@ -0,0 +1,8 @@
+void main(){
+  String pi = "3.1416";
+  //This will print pi datatype
+  print("Datatype is ${pi.runtimeType}");
+  double piDouble = double.parse(pi);
+  //This will print piDOuble datatype
+  print("Datatype of piDouble is ${piDouble.runtimeType}");
+}
