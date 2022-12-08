f3b24e3b82d5ac67023206619a723edcc2714318
Tanvir Ahammed <gxtanvir@gmail.com>
2022-12-08 15:28:09 +0600

    Create map.dart

---
+++ map.dart
@@ -0,0 +1,10 @@
+void main(){
+  Map<String, String> currency = {
+    "BD": "BDT",
+    "India": "INR",
+    "Pakistan": "PKR",
+    "USA": "USD",
+    "Russia": "Ruble"
+  };
+  print("The currency of Russia is ${currency["Russia"]}");
+}
