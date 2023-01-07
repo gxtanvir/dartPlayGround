dd76ee35ad6b9093eadeb1e1bd06e58cb247b526
Tanvir Ahammed <gxtanvir@gmail.com>
2023-01-07 12:29:15 +0600

    Create randomNum.dart

---
+++ randomNum.dart
@@ -0,0 +1,11 @@
+//Finding 10 Random Number from 1 to 8000
+
+import 'dart:math';
+
+void main(){
+  for(int i = 1; i <= 10; i++){
+    Random random = Random();
+    int lottery = random.nextInt(8000);
+    print("The $i winner is $lottery");
+  }
+}
